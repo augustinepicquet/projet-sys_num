fd9267c509f3d6d24b944dfecef43b77cbeeaec6
Gohu2000 <hugo.barzu@gmail.com>
2025-01-14 01:50:36 +0100

    Update aluinstr.py

--- aluinstr.py
+++ aluinstr.py
@@ -46,3 +46,42 @@ def mul_4(unité: Variable, dizaine: Variable):
     unité_2_mod4 = is_2(unité) | is_6(unité)
 
     return (dizaine_paire & unité_mul_4) | ((~dizaine_paire) & unité_2_mod4)
+
+def n_not(a:Variable):
+    n = a.bus_size
+    if n == 1:
+        return ~a
+    return n_not(a[0:n-1]) + (~a[n-1])
+
+def n_and(a:Variable, b:Variable):
+    assert a.bus_size == b.bus_size
+    n = a.bus_size
+    if n == 1:
+        return a & b
+    return n_and(a[0:n-1], b[0:n-1]) + (a[n-1] & b[n-1])
+
+def n_or(a:Variable, b:Variable):
+    assert a.bus_size == b.bus_size
+    n = a.bus_size
+    if n == 1:
+        return a | b
+    return n_or(a[0:n-1], b[0:n-1]) + (a[n-1] | b[n-1])
+
+def n_xor(a:Variable, b:Variable):
+    assert a.bus_size == b.bus_size
+    n = a.bus_size
+    if n == 1:
+        return a ^ b
+    return n_xor(a[0:n-1], b[0:n-1]) + (a[n-1] ^ b[n-1])
+
+def neg(a: Variable):
+    n = a.bus_size
+    return n_adder(n_not(a), Constant("0"*n), Constant("1"))[0]
+
+def mod_2(a: Variable):
+    n = a.bus_size
+    return Constant("0"*(n-1)) + a[n-1]
+
+def positif(a: Variable):
+    n = a.bus_size
+    return Constant("0"*(n-1)) + (~(a[0]))
